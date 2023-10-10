d8a2b9ee2834ca32f7ceb13bc9f406c38d9dbee7
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-10 11:11:03 +0200

    char class completed

--- texts/src/CharLessons.java
+++ texts/src/CharLessons.java
@@ -1,3 +1,41 @@
-public class CharLessons {
 
+
+
+public class CharLessons {
+public static void main(String[] args){
+  // Testing Character class
+ Character ch = 'a'; // " new Character('a'); " is deprecated
+  // System.out.println(ch);
+
+  // isLetter: static method , means can be invoked by the class name
+   System.out.println(Character.isLetter(ch));
+ // System.out.println(Character.isLetter('0'));
+
+  // isDigit : static method , means can be invoked by the class name
+  // System.out.println(Character.isDigit('B'));
+  // System.out.println(Character.isDigit('0'));
+
+  // isWhitespace: static method
+  //System.out.println(Character.isWhitespace('A')); // false
+ // System.out.println(Character.isWhitespace(' ')); // true
+  //System.out.println(Character.isWhitespace('0')); // false
+ // System.out.println(Character.isWhitespace('\n')); // true
+  //System.out.println(Character.isWhitespace('\t')); // true
+
+  // ASCII value of tab
+
+  // System.out.println(Character.isWhitespace(9)); // true : tab ASCII code
+  // System.out.println(Character.isWhitespace('9')); // false , it is a digit
+
+  // Example :
+  char c = 'b';
+  char[] charArray = {'a', 'b', 'c', 'd', 'e'};
+
+
+
+
+
+
+
+}
 }
